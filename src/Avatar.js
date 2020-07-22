81b5e7dacceb3a67a1cb0ebca93d0a0d2d6807bb
Shuofeng Wang <shuofengwang666@gmail.com>
2020-07-22 11:18:35 -0400

    Update Avatar Component: change bgcolor by initials

--- src/Avatar.js
+++ src/Avatar.js
@@ -8,6 +8,8 @@ import Image from "./Image";
 const InitialsElement = styled(Box)`
   text-transform: uppercase;
   text-align: center;
+  color: #FFFFFF;
+  background-color: ${props => props.bg };
   line-height: ${props => props.width + "px"};
 `;
 
@@ -33,22 +35,51 @@ const Avatar = forwardRef(({ src, alt, width, initials, ...props }, ref) => {
         fontSize={fontSize}
         width={width}
         height={width}
-        colorStyle={"avatar"}
+        // colorStyle={"avatar"}
+        bg={getBgColorByInitials(initials)}
         ref={ref}
         {...props}
       >
-        <Text as={"span"} colorStyle={"avatar"}>
+        <Text as={"span"}
+              color={"#FFFFFF"}
+              // colorStyle={"avatar"}
+        >
           {initials}
         </Text>
       </InitialsElement>
     );
   }
 });
+
 Avatar.displayName = "Avatar";
+
 Avatar.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string,
   initials: PropTypes.string,
   width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
+
+function getBgColorByInitials(initials) {
+  initials = initials || "";
+
+  var colors = [
+    "#5A8770", "#B2B7BB", "#6FA9AB", "#F5AF29",
+    "#0088B9", "#F18636", "#D93A37", "#A6B12E",
+    "#5C9BBC", "#F5888D", "#9A89B5", "#407887",
+    "#9A89B5", "#5A8770", "#D33F33", "#A2B01F",
+    "#F0B126", "#0087BF", "#F18636", "#0087BF",
+    "#B2B7BB", "#72ACAE", "#9C8AB4", "#5A8770",
+    "#EEB424", "#407887"
+  ];
+
+  var initialsStr = String(initials).toUpperCase();
+  var idx = 0;
+
+  for (var i = 0; i < initialsStr.length; i++)
+    idx += initialsStr.charCodeAt(i) % 26;
+
+  return colors[idx % 26];
+}
+
 export default Avatar;
--- stories/Avatar.stories.js
+++ stories/Avatar.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "../src/Avatar";
+import Box from "../src/Box";
 
 export default { title: "Avatar" };
 
@@ -14,3 +15,289 @@ export const avatar2 = () => (
 avatar2.story = {
   name: "Avatar photo",
 };
+
+export const avatarLibrary = () => (
+    <div>
+      <Box>
+        <Avatar
+            initials={"A"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"B"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"C"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"D"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"E"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"F"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"G"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"H"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"I"}
+            width={100}
+        />
+
+      </Box>
+      <br/>
+
+      <Box>
+        <Avatar
+            initials={"J"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"K"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"L"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"M"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"N"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"O"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"P"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"Q"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"R"}
+            width={100}
+        />
+      </Box>
+      <br/>
+
+      <Box>
+        <Avatar
+            initials={"S"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"T"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"U"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"V"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"W"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"X"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"Y"}
+            width={100}
+        />
+
+        <Avatar
+            initials={"Z"}
+            width={100}
+        />
+
+      </Box>
+    </div>
+);
+
+avatarLibrary.story = {
+  name: "Avatar Library",
+};
+
+// <div>
+//   <Avatar
+//       initials={"AA"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"BB"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"CC"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"DD"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"EE"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"FF"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"GG"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"HH"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"II"}
+//       width={100}
+//   />
+// </div>
+// <br/>
+//
+// <Box>
+//   <Avatar
+//       initials={"JJ"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"KP"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"LO"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"MN"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"NM"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"OL"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"PK"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"QJ"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"RI"}
+//       width={100}
+//   />
+// </Box>
+// <br/>
+//
+// <Box>
+//   <Avatar
+//       initials={"SH"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"TG"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"UF"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"VE"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"WD"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"XC"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"YB"}
+//       width={100}
+//   />
+//
+//   <Avatar
+//       initials={"ZA"}
+//       width={100}
+//   />
+// </Box>
